ca2f52ca38dbffbd6f83e315c65aa598115eebba
shamssaniat <shams.saniat@northsouth.edu>
2025-01-07 21:50:39 +0600

    Added mongoDB and Mongoose to the node.js

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 3000;
+
+export const mongoDBURL =
+    'mongodb+srv://shamssaniat:hwObVBQy6glo9Rog@bookstore-mern.sihod.mongodb.net/books.collection?retryWrites=true&w=majority&appName=BookStore-MERN'
